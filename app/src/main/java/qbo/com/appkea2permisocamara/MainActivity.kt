1529ccfeff3301cdebf50fafae461ff10b5f267d
LuisAngel <luisangel.salvat@gmail.com>
2020-11-12 20:34:56 -0500

    2da. actualización

--- app/src/main/java/qbo/com/appkea2permisocamara/MainActivity.kt
+++ app/src/main/java/qbo/com/appkea2permisocamara/MainActivity.kt
@@ -1,7 +1,12 @@
 package qbo.com.appkea2permisocamara
 
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -9,11 +14,13 @@ import android.provider.MediaStore
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
     private val CAMERA_REQUEST = 1888
@@ -77,16 +84,79 @@ class MainActivity : AppCompatActivity() {
         val fotoNombre = "JPG_$fechaHoraFoto"
         val almacenamientoDirectorio : File =
             this?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
-        return File.createTempFile(fotoNombre, ".jpg",
+        val imagen: File =  File.createTempFile(fotoNombre, ".jpg",
             almacenamientoDirectorio)
+        mRutaFoto = imagen.absolutePath
+        return imagen
     }
 
     private fun iniciarCamaraFoto(){
         val intentCamara = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         //validar que el dispositivo cuente con la aplicación de la cámara
         if(intentCamara.resolveActivity(this?.packageManager!!) != null){
-
+            val archivofoto = crearArchivoImagen()
+            if(archivofoto != null){
+                val urlfoto = FileProvider.getUriForFile(
+                        applicationContext,
+                        "qbo.com.appkea2permisocamara.provider",
+                        archivofoto
+                )
+                intentCamara.putExtra(MediaStore.EXTRA_OUTPUT, urlfoto)
+                startActivityForResult(intentCamara, CAMERA_REQUEST)
+            }
         }
+    }
+
+    private fun grabarFoto(){
+        val intentFoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val nuevoarchivo = File(mRutaFoto)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            val contenturl = FileProvider.getUriForFile(
+                    applicationContext,
+                    "qbo.com.appkea2permisocamara.provider",
+                    nuevoarchivo
+            )
+            intentFoto.data = contenturl
+        }else{
+            val contenturl = Uri.fromFile(nuevoarchivo)
+            intentFoto.data = contenturl
+        }
+        this?.sendBroadcast(intentFoto)
+    }
+
+    private fun mostrarFoto(){
+        val ei = ExifInterface(mRutaFoto)
+        val orientacion: Int = ei.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED
+        )
+        if(orientacion == ExifInterface.ORIENTATION_ROTATE_90){
+            ivfoto.rotation = 90.0F
+        }else{
+            ivfoto.rotation = 0.0F
+        }
+        val anchoiv: Int = ivfoto.width
+        val altoiv: Int = ivfoto.height
+        val bitmapOpciones = BitmapFactory.Options()
+        bitmapOpciones.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(mRutaFoto, bitmapOpciones)
+        val anchofoto: Int = bitmapOpciones.outWidth
+        val altofoto: Int = bitmapOpciones.outHeight
+        val escala: Int = min(anchoiv / anchofoto, altoiv / altofoto)
+        bitmapOpciones.inSampleSize = escala
+        bitmapOpciones.inJustDecodeBounds = false
+        val imagenbitmap = BitmapFactory.decodeFile(mRutaFoto, bitmapOpciones)
+        ivfoto.setImageBitmap(imagenbitmap)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == CAMERA_REQUEST){
+            if(resultCode == Activity.RESULT_OK){
+                grabarFoto()
+                mostrarFoto()
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 
